11999b379b54434882196605581061ce8851b89f
pcchencode <zxp791228@gmail.com>
2021-02-09 14:10:10 +0800

    update producer.sh

---
+++ producer.sh
@@ -0,0 +1,12 @@
+echo "job start"
+
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+python3 /Users/user/Desktop/python-rabbitMQ/send.py
+
+echo "job done"
+
