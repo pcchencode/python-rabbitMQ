ac1597d0656f0ffd0b778b142a314a996638ee5b
pcchencode <zxp791228@gmail.com>
2021-02-10 23:24:26 +0800

    update

--- new_task.py
+++ new_task.py
@@ -1,8 +1,7 @@
 import pika
 import sys
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
