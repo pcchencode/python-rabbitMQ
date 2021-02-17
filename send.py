4db8177f40a34cca6b3af99e83bdf35de55338ed
pcchencode <zxp791228@gmail.com>
2021-02-17 16:38:26 +0800

    update

--- send.py
+++ send.py
@@ -1,3 +1,4 @@
+import sys
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -5,6 +6,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='',routing_key='hello',body='Hello World!')
-print(" [x] Sent 'Hello World!'")
+try:
+    b_msg = sys.argv[1]
+except:
+    b_msg = "Hello World!"
+
+
+channel.basic_publish(exchange='',routing_key='hello',body=b_msg)
+print(f" [x] Sent {b_msg}")
 connection.close()
