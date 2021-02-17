a5a2b9127f14beef4b43fc415335528c7a498f65
pcchencode <zxp791228@gmail.com>
2021-02-17 15:56:15 +0800

    add sha-s56

--- insert.py
+++ insert.py
@@ -1,8 +1,20 @@
 import sys, os
 import pymysql
+import hashlib
+
 
 conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db= 'test', charset='utf8')
 cursor = conn.cursor()
+
+
+account = str(123)
+m = hashlib.md5()
+m.update(account.encode("utf-8"))
+password = m.hexdigest()
+
+print(password)
+os._exit(0)
+
 
 for i in range(1,100000):
     print(i)
